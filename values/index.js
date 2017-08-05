132a95e64c55c454aca8244367626a9ea71a6ee5
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-04 20:49:30 -0500

    updated values to take up fewer characters

--- values/index.js
+++ values/index.js
@@ -1,12 +1,9 @@
-module.exports = values;
 var forOwn = require('../for/own');
 var passesFirstArgument = require('@timelaps/fn/first');
 var bindTo = require('@timelaps/fn/bind/to');
-var keys = require('../keys');
 var push = [].push;
-
-function values(object) {
+module.exports = function values(object) {
     var collected = [];
     forOwn(object, passesFirstArgument(bindTo(push, collected)));
     return collected;
-}
+};
